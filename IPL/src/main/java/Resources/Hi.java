33ef61be5eba4c7c7a82cd04ba7d6bef7d5abe6f
Chiranjit- Workspace1 <Chiranjit@Chiranjit-PC>
2018-11-18 19:32:47 +0530

    no changes

--- IPL/src/main/java/Resources/Hi.java
+++ IPL/src/main/java/Resources/Hi.java
@@ -4,7 +4,7 @@ public class Hi {
 
 	public static void main(String[] args) {
 		System.out.println("hII..wORKSPACE");
-		System.out.println("hII..wORKSPACE2");
+		System.out.println("hII..wORKSPACE1");
 		System.out.println("hII..wORKSPACE2");
 
 	}
